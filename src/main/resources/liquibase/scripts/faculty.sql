2deaf5bccb832e4e91a8c4ebfe9fd72a898a6f6f
vachg <vachgen@yandex.ru>
2025-03-18 19:13:37 +0300

    SQL_4.3 С исправлением.

--- src/main/resources/liquibase/scripts/faculty.sql
+++
@@ -1,4 +0,0 @@
--- liquibase formatted sql
-
--- changeset krasnov:2
-CREATE INDEX id_faculty_name_color ON faculty (name, color);
--- src/main/resources/liquibase/scripts/student.sql
+++
@@ -1,4 +0,0 @@
--- liquibase formatted sql
-
--- changeset krasnov:1
-CREATE INDEX id_student_name ON student (name);
